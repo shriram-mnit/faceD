383233beb9e23be5c44c823d25ce75ff95e7f94b
shriram-mnit <43758704+shriram-mnit@users.noreply.github.com>
2020-01-23 22:39:45 +0530

    Updates

---
+++ precache-manifest.3b817731e3fa3dd610e135d602e55d9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20732fcfd30ff09199ca1a6b58c228f",
+    "url": "/faceD/index.html"
+  },
+  {
+    "revision": "3041a34767653932d6b8",
+    "url": "/faceD/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ad727318a81e222327a9",
+    "url": "/faceD/static/css/main.cf5cc0b9.chunk.css"
+  },
+  {
+    "revision": "3041a34767653932d6b8",
+    "url": "/faceD/static/js/2.1de37c45.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/faceD/static/js/2.1de37c45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad727318a81e222327a9",
+    "url": "/faceD/static/js/main.c9b227a6.chunk.js"
+  },
+  {
+    "revision": "fa73680a0b8441920c57",
+    "url": "/faceD/static/js/runtime-main.6381fc90.js"
+  }
+]);
